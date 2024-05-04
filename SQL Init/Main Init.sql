b6ce4866bb08fd4ea5ee3c75ae6e6d65e87b5db4
afrolkin <afrolkin@phoenixit.ru>
2024-05-04 21:41:25 +0300

    [UWDR-11] Добавление возможности экспорта дашбордов из графаны

--- SQL Init/Main Init.sql
+++ SQL Init/Main Init.sql
@@ -29,6 +29,8 @@ CREATE TABLE IF NOT EXISTS  main.stg_dm_weather_data_distributed ON cluster 'mai
 AS main.stg_dm_weather_data
 ENGINE = Distributed('main', main, stg_dm_weather_data, rand());
 
+
+--Таблица готовых погодных данных
 CREATE TABLE IF NOT EXISTS main.dm_weather_data on cluster 'main'
 (
 	id UUID,
@@ -50,6 +52,6 @@ CREATE TABLE IF NOT EXISTS main.dm_weather_data on cluster 'main'
 engine = ReplicatedMergeTree('/clickhouse/tabkes/{shard}/dm_weather_data_v2', '{replica}')
 ORDER BY (id, ds_id, owd_id);
 --Дистр таблица
-CREATE TABLE IF NOT EXISTS  main.dm_weather_data_distributed ON cluster 'main'
+CREATE TABLE IF NOT EXISTS main.dm_weather_data_distributed ON cluster 'main'
 AS main.dm_weather_data
 ENGINE = Distributed('main', main, dm_weather_data, rand());
