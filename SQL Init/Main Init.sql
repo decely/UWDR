a1b14df3d7824f50c2df707e5f1bc94194cafcc6
decely <danil.ratushnyj@mail.ru>
2024-05-10 14:50:06 +0300

    [UWDR-25] Добалвение загрузки DM слоя прогноза погоды с указанием разницу между create_dttm и forecast_dttm

--- SQL Init/Main Init.sql
+++ SQL Init/Main Init.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS main.stg_dm_weather_data on cluster 'main'
 	translate_dttm Nullable(DateTime),
 	lang String
 )
-engine = ReplicatedMergeTree('/clickhouse/tabkes/{shard}/stg_dm_weather_data_v2', '{replica}')
+engine = ReplicatedMergeTree('/clickhouse/tables/{shard}/stg_dm_weather_data_v2', '{replica}')
 ORDER BY (id, ds_id, owd_id);
 --Дистр таблица
 CREATE TABLE IF NOT EXISTS  main.stg_dm_weather_data_distributed ON cluster 'main'
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS main.dm_weather_data on cluster 'main'
 	translate_dttm Nullable(DateTime),
 	lang String
 )
-engine = ReplicatedMergeTree('/clickhouse/tabkes/{shard}/dm_weather_data_v2', '{replica}')
+engine = ReplicatedMergeTree('/clickhouse/tables/{shard}/dm_weather_data_v2', '{replica}')
 ORDER BY (id, ds_id, owd_id);
 --Дистр таблица
 CREATE TABLE IF NOT EXISTS main.dm_weather_data_distributed ON cluster 'main'
@@ -57,7 +57,7 @@ AS main.dm_weather_data
 ENGINE = Distributed('main', main, dm_weather_data, rand());
 
 
---STG-Таблица готовых погодных данных
+--STG-Таблица готовых данных актуального прогноза
 CREATE TABLE IF NOT EXISTS main.stg_dm_forecast_actual_data on cluster 'main'
 (
 	id UUID,
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS main.stg_dm_forecast_actual_data on cluster 'main'
 	translate_dttm Nullable(DateTime),
 	lang String
 )
-engine = ReplicatedReplacingMergeTree('/clickhouse/tabkes/{shard}/stg_dm_forecast_actual_data_v2', '{replica}', create_dttm)
+engine = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/stg_dm_forecast_actual_data_v2', '{replica}', create_dttm)
 ORDER BY (owd_id, lang, forecast_ddtm);
 --Дистр таблица
 CREATE TABLE IF NOT EXISTS  main.stg_dm_forecast_actual_data_distributed ON cluster 'main'
@@ -86,7 +86,7 @@ AS main.stg_dm_forecast_actual_data
 ENGINE = Distributed('main', main, stg_dm_forecast_actual_data, rand());
 
 
---Таблица готовых погодных данных
+--Таблица готовых данных актуального прогноза
 CREATE TABLE IF NOT EXISTS main.dm_forecast_actual_data on cluster 'main'
 (
 	id UUID,
@@ -107,9 +107,69 @@ CREATE TABLE IF NOT EXISTS main.dm_forecast_actual_data on cluster 'main'
 	translate_dttm Nullable(DateTime),
 	lang String
 )
-engine = ReplicatedReplacingMergeTree('/clickhouse/tabkes/{shard}/stg_dm_forecast_actual_data_v2', '{replica}', create_dttm)
+engine = ReplicatedReplacingMergeTree('/clickhouse/tables/{shard}/stg_dm_forecast_actual_data_v2', '{replica}', create_dttm)
 ORDER BY (owd_id, lang, forecast_ddtm);
 --Дистр таблица
 CREATE TABLE IF NOT EXISTS main.dm_forecast_actual_data_distributed ON cluster 'main'
 AS main.dm_forecast_actual_data
 ENGINE = Distributed('main', main, dm_forecast_actual_data, rand());
+
+
+--STG-Таблица готовых данных прогноза за все время
+CREATE TABLE IF NOT EXISTS main.stg_dm_forecast_all_data on cluster 'main'
+(
+	id UUID,
+	divide_id UUID,
+	ds_id UUID,
+	owd_id UUID,
+	city String,
+	temp Nullable(Float64),
+	wind_speed Nullable(Float64),
+	wind_direction String,
+	atmospheric_pressure Nullable(Int),
+	humidity Nullable(Int),
+	cloud_level Nullable(Int),
+	general_condition String,
+	forecast_diff Int,
+	forecast_ddtm DateTime,
+	create_dttm DateTime,
+	upload_dttm DateTime,
+	translate_dttm Nullable(DateTime),
+	lang String
+)
+engine = ReplicatedMergeTree('/clickhouse/tables/{shard}/stg_dm_forecast_all_data_v2', '{replica}')
+ORDER BY (id, divide_id, ds_id, owd_id);
+--Дистр таблица
+CREATE TABLE IF NOT EXISTS  main.stg_dm_forecast_all_data_distributed ON cluster 'main'
+AS main.stg_dm_forecast_all_data
+ENGINE = Distributed('main', main, stg_dm_forecast_all_data, rand());
+
+
+--Таблица готовых данных прогноза за все время
+CREATE TABLE IF NOT EXISTS main.dm_forecast_all_data on cluster 'main'
+(
+	id UUID,
+	divide_id UUID,
+	ds_id UUID,
+	owd_id UUID,
+	city String,
+	temp Nullable(Float64),
+	wind_speed Nullable(Float64),
+	wind_direction String,
+	atmospheric_pressure Nullable(Int),
+	humidity Nullable(Int),
+	cloud_level Nullable(Int),
+	general_condition String,
+	forecast_diff Int,
+	forecast_ddtm DateTime,
+	create_dttm DateTime,
+	upload_dttm DateTime,
+	translate_dttm Nullable(DateTime),
+	lang String
+)
+engine = ReplicatedMergeTree('/clickhouse/tables/{shard}/stg_dm_forecast_all_data_v2', '{replica}')
+ORDER BY (id, divide_id, ds_id, owd_id);
+--Дистр таблица
+CREATE TABLE IF NOT EXISTS main.dm_forecast_all_data_distributed ON cluster 'main'
+AS main.dm_forecast_all_data
+ENGINE = Distributed('main', main, dm_forecast_all_data, rand());
---
+++ airflow/dags/dm_dags/load_dm_forecast_all_data_dag.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+from services.dm_services.load_dm_forecast_all import (
+    truncate_stg_forecast_all_data_table,
+    load_stg_forecast_all_data_table,
+    exchange_stg_and_dm_table
+)
+
+dag_params = {
+    'dag_id': 'load_dm_forecast_all_data_dag',
+    'description': 'Даг загрузки готовых данных о прогнозе за все время из DS в DM слой',
+    'schedule_interval': None,
+    'start_date': datetime(2024, 1, 1),
+    'max_active_tasks': 1,
+    'max_active_runs': 1,
+    'catchup': False,
+    'tags': ['dm', 'forecast', 'all'],
+}
+
+
+with DAG(**dag_params) as dag:  # type: ignore
+    start = EmptyOperator(task_id='start')
+
+    truncate_stg_forecast_all_data_table = PythonOperator(
+        task_id='truncate_stg_forecast_all_data_table',
+        python_callable=truncate_stg_forecast_all_data_table,
+    )
+
+    load_stg_forecast_all_data_table = PythonOperator(
+        task_id='load_stg_forecast_all_data_table',
+        python_callable=load_stg_forecast_all_data_table,
+    )
+
+    exchange_stg_and_dm_table = PythonOperator(
+        task_id='exchange_stg_and_dm_table',
+        python_callable=exchange_stg_and_dm_table,
+    )
+
+    finish = EmptyOperator(task_id='finish')
+
+    start >> \
+        truncate_stg_forecast_all_data_table >> \
+        load_stg_forecast_all_data_table >> \
+        exchange_stg_and_dm_table >> \
+        finish
